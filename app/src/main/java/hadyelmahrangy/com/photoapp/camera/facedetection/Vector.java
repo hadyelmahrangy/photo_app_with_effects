f9044fff11a7425e61ad0e1daefc1fdf1cafa96c
roshk1n <oleh.roshka@gmail.com>
2018-06-05 21:52:22 +0300

    added vector

---
+++ app/src/main/java/hadyelmahrangy/com/photoapp/camera/facedetection/Vector.java
@@ -0,0 +1,11 @@
+package hadyelmahrangy.com.photoapp.camera.facedetection;
+
+public class Vector {
+    float x;
+    float y;
+
+    public void set(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
